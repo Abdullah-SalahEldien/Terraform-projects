b4e8f2d30c74cbd4879fdf09a3c8ea8ccaec8ba0
Abdullah-SalahEldien <84603241+Abdullah-SalahEldien@users.noreply.github.com>
2023-02-27 00:44:47 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,102 @@
+
+# Creating VPC,name, CIDR and Tags
+resource "aws_vpc" "dev" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  tags = {
+    Name = "dev"
+  }
+}
+
+# Creating Public Subnets in VPC
+resource "aws_subnet" "dev-public-1" {
+  vpc_id                  = aws_vpc.dev.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "dev-public-1"
+  }
+}
+
+
+# Creating Internet Gateway in AWS VPC
+resource "aws_internet_gateway" "dev-gw" {
+  vpc_id = aws_vpc.dev.id
+
+  tags = {
+    Name = "dev"
+  }
+}
+
+# Creating Route Tables for Internet gateway
+resource "aws_route_table" "dev-public" {
+  vpc_id = aws_vpc.dev.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dev-gw.id
+  }
+
+  tags = {
+    Name = "dev-public-1"
+  }
+}
+
+# Creating Route Associations public subnets
+resource "aws_route_table_association" "dev-public-1-a" {
+  subnet_id      = aws_subnet.dev-public-1.id
+  route_table_id = aws_route_table.dev-public.id
+}
+
+
+# Creating Security Group in public subnets
+resource "aws_security_group" "dev-sg" {
+  name = "dev-sec-grp"
+  description = "Allow HTTP and SSH traffic via Terraform"
+  vpc_id      = aws_vpc.dev.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# Creating EC2 instances in public subnets
+resource "aws_instance" "public_inst_1" {
+  ami           = "ami-0dfcb1ef8550277af"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.dev-public-1.id
+  vpc_security_group_ids = [aws_security_group.dev-sg.id]
+  associate_public_ip_address = "true"
+  key_name = "key-pair"
+  tags = {
+    Name = "public_inst_1"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install -y httpd
+    sudo systemctl start httpd.service
+    sudo systemctl enable httpd.service
+    EOF
+  }
+}
